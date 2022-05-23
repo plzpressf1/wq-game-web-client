def80d071fc5d06c1e849cec1ed1826ee8bc6597
Aleksey Serdtsev <sng.player@gmail.com>
2022-05-23 10:33:08 +0300

    added basic navigation styles

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,14 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "../styles/navigation.css";
 
+interface NavigationItem {
+    pathname: string;
+    text: string;
+}
+
+const navigation: NavigationItem[] = [
+    {
+        pathname: "/lobby",
+        text: "Игра",
+    },
+    {
+        pathname: "/games",
+        text: "Поиск",
+    },
+    {
+        pathname: "/packs",
+        text: "Паки",
+    },
+];
+
 export default function TopBar() {
+    const location = useLocation();
+
     return (
         <nav>
-            <Link to="/lobby">Игра</Link>
-            <Link to="/games">Поиск</Link>
-            <Link to="/packs">Паки</Link>
+            {
+                navigation.map(item => {
+                    const itemClasses = ["item"];
+                    if (item.pathname === location.pathname) {
+                        itemClasses.push("active");
+                    }
+                    return <div
+                        key={item.pathname}
+                        className={itemClasses.join(" ")}
+                    >
+                        <Link
+                            to={item.pathname}
+                        >
+                            {item.text}
+                        </Link>
+                    </div>;
+                })
+            }
         </nav>
     );
 }
