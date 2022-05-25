53bd73fe35f1c7df2c60d326b6c1bb9d333d23a1
Aleksey Serdtsev <sng.player@gmail.com>
2022-05-25 15:55:39 +0300

    extracted Navigation from TopBar into separate component added lobby/host navigation item dynamic switch

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { observer } from "mobx-react";
+import { Link, useLocation } from "react-router-dom";
+
+import { UserStore } from "../stores/User";
+
+import "../styles/navigation.css";
+
+interface NavigationItem {
+    pathname: string;
+    text: string;
+}
+
+const navigation: NavigationItem[] = [
+    {
+        pathname: "/lobby",
+        text: "Игра",
+    },
+    {
+        pathname: "/host",
+        text: "Захостить",
+    },
+    {
+        pathname: "/games",
+        text: "Поиск",
+    },
+    {
+        pathname: "/packs",
+        text: "Паки",
+    },
+];
+
+function Navigation() {
+    const location = useLocation();
+
+    return (
+        <nav>
+            {
+                navigation.map(item => {
+                    if (item.pathname === "/host" && UserStore.game) {
+                        return null;
+                    }
+                    if (item.pathname === "/lobby" && !UserStore.game) {
+                        return null;
+                    }
+                    const itemClasses = ["item"];
+                    if (item.pathname === location.pathname) {
+                        itemClasses.push("active");
+                    }
+                    return <div
+                        key={item.pathname}
+                        className={itemClasses.join(" ")}
+                    >
+                        <Link
+                            to={item.pathname}
+                        >
+                            {item.text}
+                        </Link>
+                    </div>;
+                })
+            }
+        </nav>
+    );
+}
+
+export default observer(Navigation);
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,51 +1,11 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
 
-import "../styles/navigation.css";
-
-interface NavigationItem {
-    pathname: string;
-    text: string;
-}
-
-const navigation: NavigationItem[] = [
-    {
-        pathname: "/lobby",
-        text: "Игра",
-    },
-    {
-        pathname: "/games",
-        text: "Поиск",
-    },
-    {
-        pathname: "/packs",
-        text: "Паки",
-    },
-];
+import Navigation from "./Navigation";
 
 export default function TopBar() {
-    const location = useLocation();
-
     return (
-        <nav>
-            {
-                navigation.map(item => {
-                    const itemClasses = ["item"];
-                    if (item.pathname === location.pathname) {
-                        itemClasses.push("active");
-                    }
-                    return <div
-                        key={item.pathname}
-                        className={itemClasses.join(" ")}
-                    >
-                        <Link
-                            to={item.pathname}
-                        >
-                            {item.text}
-                        </Link>
-                    </div>;
-                })
-            }
-        </nav>
+        <>
+            <Navigation/>
+        </>
     );
 }
