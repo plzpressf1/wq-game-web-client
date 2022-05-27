8646a9d4b6b2a9d4425b05db67126357dd07dc9c
Aleksey Serdtsev <sng.player@gmail.com>
2022-05-28 00:10:03 +0300

    added players list in Players component

--- src/components/Lobby/Players.tsx
+++ src/components/Lobby/Players.tsx
@@ -1,14 +1,35 @@
 import { IPlayer } from "./LobbyPage";
 
+import styles from "./Players.module.scss";
+
 interface PlayersProps {
     maxPlayers: number;
     players: IPlayer[];
 }
 
-export default function Players({ maxPlayers, players }: PlayersProps) {
+const prepPlayers = ({ maxPlayers, players }: PlayersProps) => {
+    const res = [];
+    for (let i = 0; i < maxPlayers; ++i) {
+        res.push({
+            key: i,
+            value: null,
+        });
+    }
+    return res;
+};
+
+export default function Players(props: PlayersProps) {
+    const players = prepPlayers(props);
     return (
-        <div>
+        <div className={styles.wrapper}>
+            {
+                players.map(player =>
+                    <div
+                        key={player.key}
+                        className={styles.player}
+                    />
+                )
+            }
         </div>
     );
 }
-
