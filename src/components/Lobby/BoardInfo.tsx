b6e5dad9185a3b77b9022045abe1677cf73bba3e
Aleksey Serdtsev <sng.player@gmail.com>
2022-05-29 22:15:56 +0300

    added basic question font auto size

--- src/components/Lobby/BoardInfo.tsx
+++ src/components/Lobby/BoardInfo.tsx
@@ -6,12 +6,25 @@ interface BoardInfoProps {
     board: FullscreenMessage[];
 }
 
+const setFit = (msg: FullscreenMessage) => {
+    if (msg.classes === undefined) return [];
+    if (msg.classes.length === 1 && msg.classes[0] === "fit") {
+        if (msg.text.length < 40) return ["h2"];
+        if (msg.text.length < 300) return ["h3"];
+        if (msg.text.length < 500) return ["h4"];
+        if (msg.text.length < 900) return ["h5"];
+        return ["h6"];
+
+    }
+    return msg.classes;
+};
+
 export default function BoardInfo({ board }: BoardInfoProps) {
     return (
         <div className="board block full-screen">
             {
                 board.map((msg, index) => {
-                    const classes = msg.classes || [];
+                    const classes = setFit(msg);
                     classes.push("message");
                     return <div
                         key={index}
